e2a961dd0839fff228b301a7259c871ece469fb6
moinkhan7402 <khan.moin7402@gmail.com>
2018-03-10 22:44:19 +0530

    Initial commit

---
+++ app/src/main/java/com/androiddialogexample/code/NewActivity.java
@@ -0,0 +1,14 @@
+package com.androiddialogexample.code;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NewActivity extends Activity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		// Get the view from new_activity.xml
+		setContentView(R.layout.new_activity);
+	}
+}
